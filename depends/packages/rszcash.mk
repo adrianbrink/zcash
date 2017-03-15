e1e9daaea8bf34e59de9f1d264da24cda3f26c3a
Sean Bowe <ewillbefull@gmail.com>
2017-03-14 20:10:26 -0600

    Include the librszcash library and call into it.

--- depends/packages/rszcash.mk
+++ depends/packages/rszcash.mk
@@ -8,10 +8,10 @@ $(package)_git_commit=2e9231b272b9a927459fbe0e08df899a645ac2dc
 $(package)_dependencies=rust
 
 define $(package)_build_cmds
-  cargo build --release
+  cargo build
 endef
 
 define $(package)_stage_cmds
   mkdir $($(package)_staging_dir)$(host_prefix)/lib/ && \
-  cp target/release/librszcash.a $($(package)_staging_dir)$(host_prefix)/lib/
+  cp target/debug/librszcash.a $($(package)_staging_dir)$(host_prefix)/lib/
 endef
