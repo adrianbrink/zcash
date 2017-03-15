48409a16966de07d84dd020caa8ff254edef05a5
Sean Bowe <ewillbefull@gmail.com>
2017-03-14 22:09:32 -0600

    Fix building and use release builds.

--- depends/packages/rszcash.mk
+++ depends/packages/rszcash.mk
@@ -3,15 +3,15 @@ $(package)_version=0.1
 $(package)_download_path=https://github.com/ebfull/dummy-zcash-rust-thing/archive/
 $(package)_file_name=$(package)-$($(package)_git_commit).tar.gz
 $(package)_download_file=$($(package)_git_commit).tar.gz
-$(package)_sha256_hash=795cedb60a5d8408f6b601b6b504641f44160814b6c5f4e6d488be9dc8b36df7
-$(package)_git_commit=2e9231b272b9a927459fbe0e08df899a645ac2dc
+$(package)_sha256_hash=a0aad9487453ed3e156d41cb70a76d277f786608986738a2662eee986b02dc2b
+$(package)_git_commit=c1278ecbd6e0db0b4b413788105e00cf61fd36c2
 $(package)_dependencies=rust
 
 define $(package)_build_cmds
-  cargo build
+  cargo build --release
 endef
 
 define $(package)_stage_cmds
   mkdir $($(package)_staging_dir)$(host_prefix)/lib/ && \
-  cp target/debug/librszcash.a $($(package)_staging_dir)$(host_prefix)/lib/
+  cp target/release/librszcash.a $($(package)_staging_dir)$(host_prefix)/lib/
 endef
--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -5,5 +5,5 @@ $(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
 $(package)_sha256_hash=b1e7c818a3cc8b010932f0efc1cf0ede7471958310f808d543b6e32d2ec748e7
 
 define $(package)_stage_cmds
-  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix) --disable-ldconfig
+  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
 endef
